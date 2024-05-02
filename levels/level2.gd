4053cdf4c9ccae1a7242815192dc960929461683
xtempore <jbriden@gmail.com>
2024-05-03 10:31:48 +1200

    Better position of coins for 2nd platform

--- levels/level2.gd
+++ levels/level2.gd
@@ -5,7 +5,7 @@ func make_items() -> void:
 		make_collectible(CollectibleSilver, 250 + 40 * i, 300 + 40 * cos(i * PI / 8))
 
 	for i in range(16):
-		make_collectible(CollectibleGold, 280 + 40 * i, 580 + 40 * cos(i * PI / 8))
+		make_collectible(CollectibleGold, 320 + 40 * i, 580 + 40 * cos(i * PI / 8))
 
 func get_time_limit() -> int:
 	return 12
